8e609d4ff59d2599d3d7b0035e945357fb1b455a
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2016-08-16 23:11:30 -0500

    Finished Raindrops

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,19 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Raindrops
+
+  @@conversions = { 3 => 'Pling',
+                    5 => 'Plang',
+                    7 => 'Plong'}
+
+
+  def self.convert number
+    answer = ''
+    @@conversions.keys.each do |factor|
+      answer << @@conversions[factor] if (number % factor).zero?
+    end
+    answer.empty? ? number.to_s : answer
+  end
+end
