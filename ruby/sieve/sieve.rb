489514612bc6a3c04d059782243eddff3d0cc3e0
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2016-08-23 22:02:09 -0500

    First pass at sieve

---
+++ ruby/sieve/sieve.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Sieve
+
+  attr_reader :num
+
+  def initialize num
+    @num = num
+  end
+
+  def primes
+    return [] if num < 2
+    list = (2..num).to_a
+    prime_arr = []
+    until list.empty? do
+      prime_arr << list.shift
+      list.map! do |x|
+        (x % prime_arr.last).zero? ? nil : x
+      end
+      list.compact!
+    end
+    prime_arr
+  end
+
+end
