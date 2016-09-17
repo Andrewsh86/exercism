cdc8e37e327f8d4be17b0fb5c946908777d5f12a
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2016-09-17 14:20:26 -0500

    First pass protein translation

---
+++ ruby/protein-translation/protein_translation.rb
@@ -0,0 +1,43 @@
+class Translation
+CHEM_MAP =   {
+    'AUG' => 'Methionine',
+    'UUU' => 'Phenylalanine',
+    'UUC'	=> 'Phenylalanine',
+    'UUA' => 'Leucine',
+    'UUG'	=> 'Leucine',
+    'UCU' => 'Serine',
+    'UCC' => 'Serine',
+    'UCA' => 'Serine',
+    'UCG'	=> 'Serine',
+    'UAU' => 'Tyrosine',
+    'UAC'	=> 'Tyrosine',
+    'UGU' => 'Cysteine',
+    'UGC'	=> 'Cysteine',
+    'UGG'	=> 'Tryptophan',
+    'UAA' => 'STOP',
+    'UAG' => 'STOP',
+    'UGA'	=> 'STOP'
+  }
+
+
+  def self.of_codon codon
+    CHEM_MAP[codon.upcase]
+  end
+
+  def self.of_rna rna
+    chems = []
+    codons = rna.chars.each_slice(3).map(&:join)
+    raise InvalidCodonError unless codons.all? { |codon| CHEM_MAP[codon]}
+    codons.each do |codon|
+      chem = of_codon(codon)
+      break if chem == 'STOP'
+      chems << chem
+    end
+    chems
+  end
+
+end
+
+class InvalidCodonError < Exception
+
+end
