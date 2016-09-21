c01696e379a388bebc6479e2d90e139af7daeb2d
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2016-09-20 20:03:06 -0500

    first pass at perfect-numbers

---
+++ ruby/perfect-numbers/perfect_numbers.rb
@@ -0,0 +1,29 @@
+class PerfectNumber
+
+  def self.classify n
+    raise RuntimeError if n <= 0
+    sum_of_factors = factors(n).inject(&:+)
+    if (sum_of_factors == n)
+      return 'perfect'
+    elsif (sum_of_factors > n)
+      return 'abundant'
+    else
+      return 'deficient'
+    end
+  end
+
+  def self.factors n
+    1.upto(n/2).inject([]) do |factors, candidate|
+      if (n % candidate).zero?
+        factors << candidate
+        factors << (n / candidate) unless candidate == 1
+      end
+      factors.uniq
+    end
+  end
+
+end
+
+module BookKeeping
+  VERSION = 1
+end
