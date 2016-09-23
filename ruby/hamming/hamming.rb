2f709ef44680d915df3cdbc30bbb71150e40ca39
Andrew Heykamp <Andrew.Heykamp@gmail.com>
2016-09-23 10:12:21 -0500

    smarter hamming

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -6,11 +6,8 @@ class Hamming
 
   def self.compute seq_1, seq_2
     raise ArgumentError if seq_1.length != seq_2.length
-    count = 0
-    seq_1.chars.each_with_index do |elem, index|
-      count += 1 if seq_2[index] != elem
-    end
-    count
+    pairs = seq_1.chars.zip(seq_2.chars)
+    pairs.count { |pair| pair.first != pair.last}
   end
 
 end
