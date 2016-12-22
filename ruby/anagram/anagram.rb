cf92bfbb79d0383b0eea254e8c79203d082b0cad
Andrewsh86 <andrew.heykamp@gmail.com>
2016-12-22 12:27:33 -0600

    Update anagram.rb

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -2,12 +2,12 @@ class Anagram
 
   def initialize word
     @word = word
+    @sorted_word = @word.downcase.chars.sort.join
   end
 
   def match words
-    sorted_word = @word.downcase.chars.sort.join
-    words.delete_if { |word| word.downcase == @word.downcase}
-    words.select { |word| word.downcase.chars.sort.join == sorted_word}
+    dup_words = words.dup
+    dup_words.select { |word| word != @word && word.downcase.chars.sort.join == @sorted_word}
   end
 
 end
